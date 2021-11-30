87c2e412baf81e523e494da46e73546ef5906eb9
Eli <elisadelap00@gmail.com>
2021-11-30 14:09:50 +0100

    Anubis-preEscenario

--- Anubis/Assets/scripts/AnubisMove.cs
+++ Anubis/Assets/scripts/AnubisMove.cs
@@ -25,14 +25,40 @@ public class AnubisMove : MonoBehaviour
 
     void Salto()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && ani.GetBool("IsGrounded"))
         {
             ani.SetTrigger("Jump");
             ani.SetBool("IsGrounded",false);
             ani.SetBool("IsFalling", false);
             Vector2 dirSalto = new Vector2(0f, 1f);
-            rb.AddForce(dirSalto * fuerza);
+            rb.AddForce(dirSalto * fuerza, ForceMode2D.Impulse);
+            
+        }
+
+        if (rb.velocity.y < 0.15f)
+        {
             ani.SetBool("IsFalling", true);
         }
+
+        //Vector3 rayOrigin = transform.position + new Vector3(0.7f, 2.8f, 0f);
+        Vector3 rayOrigin = transform.position + new Vector3(0f, 0f, 0f);
+        Debug.DrawRay(rayOrigin, Vector3.down * groundDist, Color.red);
+
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, groundDist);
+        // If it hits something...
+        if (hit.collider != null)
+        {
+            ani.SetBool("IsFalling", false);
+            ani.SetBool("IsGrounded", true);
+
+            print("Estoy chocando con " + hit.collider.name);
+
+        }
+        else
+        {
+            ani.SetBool("IsGrounded", false);
+        }
     }
+
 }
+
